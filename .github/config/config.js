5423bf156c4c36cf71448d68168b2bf8e9d627b2
Andrei Liviu Georgescu <andrei@andreigeorgescu.com>
2022-09-06 10:02:47 -0400

    Update config.js

--- .github/config/config.js
+++ .github/config/config.js
@@ -1,15 +1,17 @@
-'use strict'
-const config = require('conventional-changelog-conventionalcommits');
-
-module.exports = config({
-    "types": [
-    {"type": "feat", "section": "🚀 Features"},
-    {"type": "fix", "section": "Bug Fixes"},
-    {"type": "chore", "hidden": true},
-    {"type": "docs", "hidden": true},
-    {"type": "style", "hidden": true},
-    {"type": "refactor", "hidden": true},
-    {"type": "perf", "hidden": true},
-    {"type": "test", "hidden": true}
-]
-})
+{
+    "options": {
+        "preset": {
+            "name": "conventionalchangelog",
+            "types": [
+                {"type": "feat", "section": "🚀 Features"},
+                {"type": "fix", "section": "Bug Fixes"},
+                {"type": "chore", "hidden": true},
+                {"type": "docs", "hidden": true},
+                {"type": "style", "hidden": true},
+                {"type": "refactor", "hidden": true},
+                {"type": "perf", "hidden": true},
+                {"type": "test", "hidden": true}
+            ]
+        }
+    }
+}
